74a9e6e82d86832c80c621d09a2ff2f8d151a239
TheCodeMage <mr.phyoekyawkyaw@gmail.com>
2025-08-06 15:11:21 +0630

    5th Commit

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
+    path('shop/product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
